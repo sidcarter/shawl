443b19c1f42023af02c5cd2f8f9e995ba4c06c00
Sid Carter <me@sidcarter.com>
2015-03-18 22:10:09 -0400

    a few minor changes for testing

--- src/main/scala/Shawl.scala
+++ src/main/scala/Shawl.scala
@@ -8,49 +8,53 @@ import com.gistlabs.mechanize.document.html.form.Form
 import com.gistlabs.mechanize.impl.MechanizeAgent
 
 object Shawl extends App {
+	
 	val cloth = new Wool()
-
+	
 	val username = cloth.getUsername("username: ")
 	val password = cloth.getPassword("password: ")
-
-	val agent= new MechanizeAgent()
-	agent.setUserAgent(cloth.userAgent)
-		
-	val page:AbstractDocument  = agent.get(cloth.schwabThread)
+	
+	val weaver= new MechanizeAgent()
+	weaver.setUserAgent(cloth.userAgent)
+			
+	var page:AbstractDocument = weaver.get(cloth.schwabThread)
 //	println(page)
-
+	
 	val form = page.form("SignonForm")
 
 	form.get("SignonAccountNumber").set(username)
 	form.get("SignonPassword").set(password)
 	val response:AbstractDocument= form.submit()
 
-	println(response) // for debug
-
+//	println(response) // for debug
+	
 	if (response.getUri().endsWith("YES")) {
 		println("Authentication succeeded.") 
 		val cookieStore = response.getAgent().cookies().getAll()
-		for (cookie <- cookieStore if cookie.getDomain().startsWith("client")) {
+		for (cookie <- cookieStore) {
 			cloth.cookie = (cookie.getValue())
 			println (s"Name: ${cookie.getName()}")
 			println (s"Domain: ${cookie.getDomain()}")
-			println (s"Value: ${cloth.cookie}")
+			println (s"Value: ${cloth.cookie}\n")
 		}
 
-//		val reAgent = new MechanizeAgent()
-//		reAgent.setUserAgent(userAgent)
-//		reAgent.get(response.getUri())
+//		val reWeaver = new MechanizeAgent()
+//		reWeaver.setUserAgent(cloth.userAgent)
+//		page = reWeaver.get(response.getUri())
+		
+//		val htmlElement:HtmlElement = page.htmlElements().find("span#ctl00_wpm_ac_ac_rba_ctl00_dsv")
+		
 	}
 	// /Accounts/Summary/Summary.aspx?ShowUN=YES
 	else if (response.getUri().endsWith("ErrorInvalidCredentials")) println("Authentication failed.")
 	// /Login/SignOn/CustomerCenterLogin.aspx?ErrorCode=ErrorInvalidCredentials
 	else println("Unknown authentication error.") 
 	// /Login/SignOn/CustomerCenterLogin.aspx?ErrorCode=Login-Error-DWTThirdAttemptFailed
-	
+		
 	/**
 	println(form)
 	println(response.getTitle())
-	val page:AbstractDocument  = agent.get("https://client.schwab.com/Login/SignOn/CustomerCenterLogin.aspx")
+	val page:AbstractDocument  = weaver.get("https://client.schwab.com/Login/SignOn/CustomerCenterLogin.aspx")
 	val usernameField: String = "ctl00$WebPartManager1$CenterLogin$LoginUserControlId$txtLoginID"
 	val response:AbstractDocument= form.submit()
 	val action = page.action("https://client.schwab.com/Login/SignOn/signon.ashx")
